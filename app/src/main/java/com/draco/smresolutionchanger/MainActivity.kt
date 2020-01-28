c7d2d82a0b1e9e8d7abac62cd19e16ae574da45e
tytydraco <tylernij@gmail.com>
2020-01-27 22:12:34 -0800

    Initial refactor
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/smresolutionchanger/MainActivity.kt
+++ app/src/main/java/com/draco/smresolutionchanger/MainActivity.kt
@@ -1,50 +1,245 @@
 package com.draco.smresolutionchanger
 
-import android.content.Context
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.os.CountDownTimer
+import android.os.Handler
+import android.preference.PreferenceManager
+import android.util.DisplayMetrics
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
-
-lateinit var sharedPrefs: SharedPreferences
-lateinit var editor: SharedPreferences.Editor
-lateinit var width: EditText
-lateinit var height: EditText
-lateinit var apply: Button
-lateinit var def: Button
-lateinit var manualRefresh: Button
-lateinit var density: EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import kotlin.concurrent.fixedRateTimer
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
+    /* Command that user must run to grant permissions */
+    private val adbCommand = "pm grant ${BuildConfig.APPLICATION_ID} android.permission.WRITE_SECURE_SETTINGS"
+
+    /* Preferences */
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    /* Internal classes */
+    private lateinit var wmApi: WmApi
+
+    /* UI elements */
+    private lateinit var width: EditText
+    private lateinit var height: EditText
+    private lateinit var density: EditText
+    private lateinit var reset: Button
+    private lateinit var apply: Button
+
+    object DefaultScreenSpecs {
+        var width: Int = 0
+        var height: Int = 0
+        var density: Int = 0
+        var diagInches: Int = 0
+        var diagPixels: Int = 0
+    }
+
+    /* Set DefaultScreenSpecs to current settings */
+    private fun setupDefaultScreenSpecs() {
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+
+        DefaultScreenSpecs.width = dm.widthPixels
+        DefaultScreenSpecs.height = dm.heightPixels
+        DefaultScreenSpecs.density = dm.densityDpi
+
+        val wi = dm.widthPixels.toDouble() / dm.xdpi.toDouble()
+        val hi = dm.heightPixels.toDouble() / dm.ydpi.toDouble()
+
+        DefaultScreenSpecs.diagInches = sqrt(wi.pow(2.0) + hi.pow(2.0)).toInt()
+        DefaultScreenSpecs.diagPixels = sqrt(dm.widthPixels.toDouble().pow(2) +
+                dm.heightPixels.toDouble().pow(2)).toInt()
+    }
+
+    /* Estimate proper DPI for device */
+    private fun calculateDPI(x: Int, y: Int): Int {
+        val diagPixels = sqrt(x.toDouble().pow(2) + y.toDouble().pow(2)).toInt()
+        return (diagPixels / DefaultScreenSpecs.diagInches)
+    }
+
+    /* Apply resolution and density */
+    private fun apply() {
+        var w = 0
+        var h = 0
+        var d = 0
+
+        w = try {
+            Integer.parseInt(width.text.toString())
+        } catch (_: Exception) {
+            DefaultScreenSpecs.width
+        }
+
+        h = try {
+            Integer.parseInt(height.text.toString())
+        } catch (_: Exception) {
+            DefaultScreenSpecs.height
+        }
+
+        d = try {
+            Integer.parseInt(density.text.toString())
+        } catch (_: Exception) {
+            calculateDPI(w, h)
+        }
+
+        wmApi.setBypassBlacklist(true)
+        wmApi.setDisplayResolution(w, h)
+        wmApi.setDisplayDensity(d)
+
+        Handler().postDelayed({
+            showWarningDialog()
+        }, 500)
+
+        updateEditTexts()
+    }
+
+    /* Show 5 second countdown */
+    private fun showWarningDialog() {
+        var dismissed = false
+
+        val dialog = AlertDialog.Builder(this)
+                .setTitle("Confirm Settings")
+                .setMessage("Resetting in 5 seconds.")
+                .setPositiveButton("Confirm") { _: DialogInterface, _: Int ->
+                    dismissed = true
+                }
+                .setCancelable(false)
+                .create()
+
+        dialog.show()
+
+        object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (dismissed) {
+                    this.cancel()
+                } else
+                    dialog.setMessage("Resetting in " + ((millisUntilFinished / 1000) + 1) + " seconds.")
+            }
+
+            override fun onFinish() {
+                if (!dismissed) {
+                    dialog.dismiss()
+                    reset()
+                }
+            }
+        }.start()
+    }
+
+    /* Use new resolution for text */
+    private fun updateEditTexts() {
+        /* Read screen specs */
+        setupDefaultScreenSpecs()
+
+        width.setText(DefaultScreenSpecs.width.toString())
+        height.setText(DefaultScreenSpecs.height.toString())
+        density.setText(DefaultScreenSpecs.density.toString())
+    }
+
+    /* Reset resolution and density */
+    private fun reset() {
+        wmApi.setBypassBlacklist(true)
+        wmApi.clearDisplayDensity()
+        wmApi.clearDisplayResolution()
+
+        updateEditTexts()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sharedPrefs = getSharedPreferences("SMResolutionChanger", Context.MODE_PRIVATE)
-        editor = sharedPrefs.edit()
+        /* Configure classes for our activity */
+        wmApi = WmApi(contentResolver)
 
+        /* Setup preferences */
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        editor = sharedPreferences.edit()
+
+        /* Setup UI elements */
         width = findViewById(R.id.width)
         height = findViewById(R.id.height)
-        apply = findViewById(R.id.apply)
-        def = findViewById(R.id.def)
-        manualRefresh = findViewById(R.id.manual_refresh)
         density = findViewById(R.id.density)
+        reset = findViewById(R.id.reset)
+        apply = findViewById(R.id.apply)
 
-        apply.setOnClickListener { applyResolution(this, getTextResolution(), getTextDensity()) }
-        def.setOnClickListener { defaultResolution(this) }
-        manualRefresh.setOnClickListener { refreshViews(this) }
+        reset.setOnClickListener {
+            reset()
 
-        permissionCheck(this)
+            reset.startAnimation(AnimationUtils.loadAnimation(this, R.anim.press))
+        }
+
+        apply.setOnClickListener {
+            apply()
+
+            apply.startAnimation(AnimationUtils.loadAnimation(this, R.anim.press))
+        }
+
+        /* Request or confirm if we can perform proper commands */
+        checkPermissions()
+
+        /* Show the current display config */
+        updateEditTexts()
+
+        /* Activate dark mode if the system is dark themed */
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 
-    private fun getTextDensity(): String {
-        return density.text.toString()
+    private fun hasPermissions(): Boolean {
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_SECURE_SETTINGS)
+        return permissionCheck == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun getTextResolution(): String {
-        return width.text.toString() + "x" + height.text.toString()
+    private fun checkPermissions() {
+        if (hasPermissions())
+            return
+
+        val dialog = AlertDialog.Builder(this)
+                .setTitle("Missing Permissions")
+                .setMessage(getString(R.string.adb_tutorial) + "adb shell $adbCommand")
+                .setPositiveButton("Check Again", null)
+                .setNeutralButton("Setup ADB", null)
+                .setCancelable(false)
+                .create()
+
+        dialog.setOnShowListener {
+            /* We don't dismiss on Check Again unless we actually have the permission */
+            val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            positiveButton.setOnClickListener {
+                if (hasPermissions())
+                    dialog.dismiss()
+            }
+
+            /* Open tutorial but do not dismiss until user presses Check Again */
+            val neutralButton = dialog.getButton(AlertDialog.BUTTON_NEUTRAL)
+            neutralButton.setOnClickListener {
+                val uri = Uri.parse("https://www.xda-developers.com/install-adb-windows-macos-linux/")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+        }
+
+        dialog.show()
+
+        /* Check every second if the permission was granted */
+        fixedRateTimer("permissionCheck", false, 0, 1000) {
+            if (hasPermissions()) {
+                dialog.dismiss()
+                this.cancel()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/draco/smresolutionchanger/WmApi.kt
@@ -0,0 +1,116 @@
+package com.draco.smresolutionchanger
+
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.provider.Settings
+import android.view.Display
+import java.lang.Exception
+
+class WmApi(private val contentResolver: ContentResolver) {
+
+    /* User ID to pass to WindowManager APIs */
+    private val userId = -3
+
+    /* List of Global settings that allow blacklisted APIs to be called */
+    private val blacklistGlobalSettings = listOf(
+            "hidden_api_policy",
+            "hidden_api_policy_pre_p_apps",
+            "hidden_api_policy_p_apps"
+    )
+
+    /* If true, allow blacklisted APIs to be called */
+    fun setBypassBlacklist(mode: Boolean) {
+        when (mode) {
+            true -> {
+                for (setting in blacklistGlobalSettings) {
+                    Settings.Global.putInt(contentResolver, setting, 1)
+                }
+            }
+            false -> {
+                for (setting in blacklistGlobalSettings) {
+                    Settings.Global.putInt(contentResolver, setting, 0)
+                }
+            }
+        }
+    }
+
+    /* Fetch the system WindowManager service */
+    @SuppressLint("PrivateApi")
+    private fun getWindowManagerService(): Any? {
+        var wmService: Any? = null
+
+        try {
+            wmService = Class.forName("android.view.WindowManagerGlobal")
+                    .getMethod("getWindowManagerService")
+                    .invoke(null)
+        } catch (_: Exception) {}
+
+        return wmService
+    }
+
+    /* Set display resolution */
+    @SuppressLint("PrivateApi")
+    fun setDisplayResolution(x: Int, y: Int) {
+        /* Return if we can't get WindowManager service */
+        val wmService = getWindowManagerService() ?: return
+
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("setForcedDisplaySize", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY, x, y)
+        } catch (_: Exception) {}
+    }
+
+    /* Clear display resolution */
+    @SuppressLint("PrivateApi")
+    fun clearDisplayResolution() {
+        /* Return if we can't get WindowManager service */
+        val wmService = getWindowManagerService() ?: return
+
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("clearForcedDisplaySize", Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY)
+        } catch (_: Exception) {}
+    }
+
+    /* Set display density */
+    @SuppressLint("PrivateApi")
+    fun setDisplayDensity(density: Int) {
+        /* Return if we can't get WindowManager service */
+        val wmService = getWindowManagerService() ?: return
+
+        /* Try the old API for some devices */
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("setForcedDisplayDensity", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY, density)
+        } catch (_: Exception) {}
+
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("setForcedDisplayDensityForUser", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY, density, userId)
+        } catch (_: Exception) {}
+    }
+
+    /* Clear display density */
+    @SuppressLint("PrivateApi")
+    fun clearDisplayDensity() {
+        /* Return if we can't get WindowManager service */
+        val wmService = getWindowManagerService() ?: return
+
+        /* Try the old API for some devices */
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("clearForcedDisplayDensity", Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY)
+        } catch (_: Exception) {}
+
+        try {
+            Class.forName("android.view.IWindowManager")
+                    .getMethod("clearForcedDisplayDensityForUser", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+                    .invoke(wmService, Display.DEFAULT_DISPLAY, userId)
+        } catch (_: Exception) {}
+    }
+}
